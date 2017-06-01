31d95ba8e7ef64ba8fd9b6f6f4e1577a6d8d1bb4
nnnnnnnnnnnaeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeepttttttttttttttttttttttttttttttttttthuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuubnnnnnnnnnnnnnnnnn <nyantaa@protonmail.com>
2017-06-01 08:07:44 -0400

    BondPro.sh
    BondPro, but now only (almost) one line of code! Just type '. BondPro.sh' or 'source BondPro.sh' while in it's directory to run it.

---
+++ BondPro.sh
@@ -0,0 +1,29 @@
+sort -R <<EOF | head -n 1
+1. Climax! (1954) / 5.8/10 / William Lundigan
+2. Dr. No (1962) / 7.3/10 / Sean Connery
+3. From Russia.. / 7.5/10 / Sean Connery
+4. Goldfinger   / 7.8/10 / Sean Connery
+5. Thunderball / 7.0/10 / Sean Connery
+6. You Only L2x / 6.9/10 / Sean Connery
+7. Casino Royale / 5.2/10 / David Nivan
+8. On Her Majes~ / 6.8/10 / George Lazenby
+9. DiamondsR4eva / 6.7/10 / Sean Connery
+10.Live&letdie  / 6.8/10 / Roger Moore
+11.guyWgoldgun  / 6.8/10 / Roger Moore
+12.thespywholuv / 7.1/10 / Roger Moore
+13.Moonraker    / 6.3/10 / Roger Moore
+14.ForyourI'snly/ 6.8/10 / Roger Moore
+15.Octopussy / 6.6/10 / Roger Moore
+16.nvrsaynvragain / 6.2/10 / Sean Connery
+17.Aview2kill / 6.3/10 / Roger Moore
+18.tehlivndaylghts / 6.7/10 / Timothy Dalton
+19.Licence2kill / 6.6/10 / Timothy Dalton
+20.goldeneye / 7.2/10 / pierce vrosnan
+21.tmrnvrdies / 6.5/10 / pierce brosnam
+22.thisworldnotnuf / 6.4/10 / pierce brosnan
+23.Dieanothaday / 6.1/10 / pierce brosnan
+24.CasinoRoyale/ 8.0/10 / dan craig
+25.quantsol / 6.6/10 / dan craig
+26.skyfall / 7.8/10 / dan craig
+27.spectre / 6.8/10 / dan craig
+EOF
